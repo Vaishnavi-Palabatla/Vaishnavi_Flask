22860d75a7a2069f04aece9089703e2d8d6a22f1
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-07-17 15:08:58 +0530

    with html pages

--- day3(practise_flask)/app.py
+++ day3(practise_flask)/app.py
@@ -1,12 +1,16 @@
-from flask import Flask;
+from flask import Flask,render_template;
 
 app=Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello world"
+# @app.route("/")
+# def index():
+#     return "Hello world"
 
 @app.route("/<name>")
 def index1(name):
-    return f"Hello {name}"
+    return render_template("index.html", name=name)
+
+# @app.route("/")
+# def index1():
+#     return render_template("index.html")
 
