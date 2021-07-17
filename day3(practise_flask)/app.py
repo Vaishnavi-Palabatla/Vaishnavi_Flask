b8a29f21244779fafe2732d988b85585b4b3a2b2
Vaishnavi-Palabatla <vaishnavi136@msitprogram.net>
2021-07-17 16:03:34 +0530

    logout page

--- day3(practise_flask)/app.py
+++ day3(practise_flask)/app.py
@@ -1,16 +1,20 @@
-from flask import Flask,render_template;
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     return "Hello world"
+@app.route("/")
+def demo():
+    return "Hello world"
 
 @app.route("/<name>")
-def index1(name):
-    return render_template("index.html", name=name)
+def index(name):
+    return render_template("index.html",name=name)
 
-# @app.route("/")
-# def index1():
-#     return render_template("index.html")
+# @app.route("/<name>")
+# def index1(name):
+#     return render_template("index.html", name=name)
+
+@app.route("/logout1")
+def logout1():
+    return render_template("logout1.html")
 
